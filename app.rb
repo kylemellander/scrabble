38c2c944765648aa85d3bd256be35067ce9c2fbd
Josue Valverde & Kyle Mellander <student@epicodus.com>
2015-08-05 11:56:46 -0700

    Add custom styling and variable fetches in app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/scrabble')
+also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/scrabble') do
+  @word = params.fetch('word')
+  @score = @word.scrabble()
+  erb(:scrabble)
 end
